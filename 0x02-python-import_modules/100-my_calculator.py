841325f1a3257b99855bce50b24f2b64ba61ebaa
AdrianVides56 <adrianvj18@gmail.com>
2020-12-15 02:09:15 +0000

    Fixed pep8 style

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,10 +6,10 @@ if __name__ == "__main__":
 operators = {'+', '-', '*', '/'}
 if len(sys.argv) != 4:
     print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    exit (1)
+    exit(1)
 if (sys.argv[2] not in operators):
     print("Unknown operator. Available operators: +, -, * and /")
-    exit (1)
+    exit(1)
 else:
     if sys.argv[2] == '+':
         print("{} + {} = {}".format(sys.argv[1], sys.argv[3],
