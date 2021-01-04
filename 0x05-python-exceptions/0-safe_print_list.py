c6169199f1cb1fa9ebaf91973aee2adae5f9d74f
AdrianVides56 <adrianvj18@gmail.com>
2021-01-04 15:54:40 -0500

    Added 0-safe_print_list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    idx = 0
+    try:
+        for a in range(x):
+            print(my_list[a], end="")
+            idx += 1
+    except IndexError:
+        None
+    print()
+    return idx
