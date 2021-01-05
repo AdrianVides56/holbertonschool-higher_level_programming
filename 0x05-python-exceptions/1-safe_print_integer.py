2ec6e1116432559664d94d0fd6d44f01e0fd71b0
AdrianVides56 <adrianvj18@gmail.com>
2021-01-04 19:48:52 -0500

    Added condition if value is None

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
+    if value is None:
+        return False
     try:
         print("{:d}".format(value))
-    except ValueError:
+    except (ValueError):
         return False
     return True
