a83e927121e4f903c30b86af96228352991d4c24
AdrianVides56 <adrianvj18@gmail.com>
2021-01-05 21:50:37 -0500

    Added 3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Creating a class"""
+
+
+class Square:
+    def __init__(self, size=0):
+        self.__size = size
+        """initializing size as private attribute"""
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        """Check if size is not an integer"""
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        """Checks id size if lower than zero"""
+
+    def area(self):
+        """defining a method to calc the square area"""
+        return self.__size ** 2
