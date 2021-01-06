ba003379806fe176b346bf8de658bbfebdb1fe13
AdrianVides56 <adrianvj18@gmail.com>
2021-01-05 22:34:33 -0500

    Documenting

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -7,25 +7,26 @@ class Square:
 
     def __init__(self, size=0):
         self.__size = size
-
+        """initializing size as private attribute"""
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
 
-    def area(self):
-        return self.__size ** 2
-
     @property
     def size(self):
+        """gets the value of the attribute size"""
         return self.__size
-    """getter"""
 
     @size.setter
     def size(self, value):
+        """updates the attribute size"""
         self.__size = value
-        """setter"""
         if type(value) is not int:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
+
+    def area(self):
+        """defining a method to calc the square area"""
+        return self.__size ** 2
