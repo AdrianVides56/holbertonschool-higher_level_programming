50533a4b519441ea66f6ca90d023118405b98de1
AdrianVides56 <adrianvj18@gmail.com>
2021-01-11 16:43:07 -0500

    Added 2-rectangle.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -4,24 +4,9 @@
 
 class Rectangle:
     """Defines a Rectangle class"""
-    def __init__(self, width=0, heigth=0):
-        """Init values"""
-        self.__heigth = heigth
-        self.__width = width
-
-    @property
-    def heigth(self):
-        """gets the heigth of the rectangle"""
-        return self.__heigth
-
-    @heigth.setter
-    def heigth(self, value):
-        """Sets the heigth of the rectangle"""
-        if type(value) != int:
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__heigth = value
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
@@ -37,11 +22,24 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.__width = value
 
+    @property
+    def height(self):
+        """gets the height of the rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets the height of the rectangle"""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
     def area(self):
         """Calcs the rectangle's area"""
-        print("heigth", self.heigth, "width", self.width)
-        return self.__heigth * self.__width
+        return self.__height * self.__width
+
     def perimeter(self):
         """Calcs the rectangle's perimeter"""
-#        print("heigth", self.heigth, "width", self.width)
-        return (self.__heigth * 2) + (self.__width * 2)
+        return (self.__height * 2) + (self.__width * 2)
