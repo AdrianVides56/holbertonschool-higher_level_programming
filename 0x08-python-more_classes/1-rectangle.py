ab044bdfc2d9064737c5b9965990f5735cb43e25
AdrianVides56 <adrianvj18@gmail.com>
2021-01-11 18:46:05 -0500

    Changed order of __init__ function

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,8 +5,8 @@
 class Rectangle:
     """Defines a Rectangle class"""
     def __init__(self, width=0, height=0):
-        self.width = width
         self.height = height
+        self.width = width
 
     @property
     def width(self):
