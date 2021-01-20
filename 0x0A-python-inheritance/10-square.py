603b9480c41c334c69656cbd3158e58f63820996
AdrianVides56 <adrianvj18@gmail.com>
2021-01-19 22:55:45 -0500

    Added task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" New module """
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+"""Import Rectangle module"""
+
+
+class Square(Rectangle):
+    """Creates new sublass"""
+    def __init__(self, size):
+        """Initial values"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
