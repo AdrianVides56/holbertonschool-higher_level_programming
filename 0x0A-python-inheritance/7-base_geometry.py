80ff1f7d52368aaa8278fe572ec66f189bedc0bb
AdrianVides56 <adrianvj18@gmail.com>
2021-01-19 21:25:04 -0500

    Fixing pep8 style and raise statement

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-""" Creates a class """
+"""Creates a class"""
 
 
 class BaseGeometry:
-    """ class """
+    """class"""
 
     def area(self):
-        """ Empty method """
+        """Empty method"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """ Validates value """
+        """Validates value"""
         if type(value) is not int:
             raise TypeError(name + " must be an integer")
-        if value < 1:
+        if value <= 0:
             raise ValueError(name + " must be greater than 0")
