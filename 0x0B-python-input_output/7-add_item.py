e72991d224377c19c95f2b126bae1b169103b1d6
AdrianVides56 <adrianvj18@gmail.com>
2021-01-21 20:40:42 -0500

    Added task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Initializes module and import"""
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+from sys import argv
+
+
+my_list = []
+my_list = load_from_json_file('add_item.json')
+
+for i in range (1, len(argv)):
+    my_list.append(argv[i])
+
+save_to_json_file(my_list, 'add_item.json')
