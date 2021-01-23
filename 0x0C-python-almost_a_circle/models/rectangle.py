f0aac1695038ac40814aaca4a19338a1f685d479
AdrianVides56 <adrianvj18@gmail.com>
2021-01-23 14:05:31 -0500

    Added task 8

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -84,3 +84,24 @@ class Rectangle(Base):
         """ Returns rectangle information"""
         return ("[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.__x, self.__y, self.__width, self.__height))
+
+    def update(self, *args):
+        """
+        Updates the rectangle:
+        - 1st argument should be the id attribute
+        - 2nd argument should be the width attribute
+        - 3rd argument should be the height attribute
+        - 4th argument should be the x attribute
+        - 5th argument should be the y attribute
+        """
+        for count in range(len(args)):
+            if count == 0:
+                self.id = args[0]
+            if count == 1:
+                self.__width = args[1]
+            if count == 2:
+                self.__height = args[2]
+            if count == 3:
+                self.__x = args[3]
+            if count == 4:
+                self.__y = args[4]
