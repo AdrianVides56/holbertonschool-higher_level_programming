dcd23723f9ee8141dfc3e8af5771bb449a36dd09
AdrianVides56 <adrianvj18@gmail.com>
2021-01-26 22:29:51 -0500

    Added unittests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -10,10 +10,10 @@ class Test_Rectangle(unittest.TestCase):
     def test_regular_working(self):
         """ Correc working """
         r1 = Rectangle(10, 2)
-        self.assertEqual(r1.id, 2)
+        self.assertEqual(r1.id, 1)
 
         r2 = Rectangle(2, 10)
-        self.assertEqual(r2.id, 3)
+        self.assertEqual(r2.id, 2)
 
         r3 = Rectangle(10, 2, 0, 0, 12)
         self.assertEqual(r3.id, 12)
