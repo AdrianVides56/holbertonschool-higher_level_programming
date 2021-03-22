331eebecc086c0ba4cedfb7eb29822d54b95e81e
AdrianVides56 <adrianvj18@gmail.com>
2021-03-22 17:13:16 -0500

    Fixing semistandard style

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,14 +2,13 @@
 let idx = 0;
 let row = '';
 if (isNaN(process.argv[2])) {
-    console.log('Missing size');
-}
-else {
-    for (idx = 0; idx < process.argv[2]; idx++) {
+  console.log('Missing size');
+} else {
+  for (idx = 0; idx < process.argv[2]; idx++) {
     row += 'X';
-    }
+  }
 
-    for (idx = 0; idx < process.argv[2]; idx++) {
+  for (idx = 0; idx < process.argv[2]; idx++) {
     console.log(row);
-    }
+  }
 }
