bbf98eefa78f03543c24307a2d47a2718d4f345f
AdrianVides56 <adrianvj18@gmail.com>
2021-03-22 16:36:02 -0500

    Changed output

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const myArg = process.argv[2];
-if (isNaN(myArg) === false) {
+if (!isNaN(myArg)) {
   console.log('My number: ' + myArg);
 } else {
-  console.log('Not a Number');
+  console.log('Not a number');
 }
