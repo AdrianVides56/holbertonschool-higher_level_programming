c026a5bbf0c4bcdca122490ccca272b2a9c5b6a3
AdrianVides56 <adrianvj18@gmail.com>
2021-03-22 17:12:14 -0500

    Added conditional if isNaN

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,15 @@
 #!/usr/bin/node
 let idx = 0;
 let row = '';
-for (idx = 0; idx < process.argv[2]; idx++) {
-  row += 'X';
+if (isNaN(process.argv[2])) {
+    console.log('Missing size');
 }
+else {
+    for (idx = 0; idx < process.argv[2]; idx++) {
+    row += 'X';
+    }
 
-for (idx = 0; idx < process.argv[2]; idx++) {
-  console.log(row);
+    for (idx = 0; idx < process.argv[2]; idx++) {
+    console.log(row);
+    }
 }
