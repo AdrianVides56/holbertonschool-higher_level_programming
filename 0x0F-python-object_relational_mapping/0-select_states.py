2e553c7d80a96a2d3896611931b052854a1681bd
AdrianVides56 <adrianvj18@gmail.com>
2021-03-27 21:25:35 -0500

    Fixed pep8 style

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,22 +4,22 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-  """ lists states from hbtn_0a_usa db """
-  db = MySQLdb.connect(host='localhost',
-                       port=3306,
-                       user=argv[1],
-                       password=argv[2],
-                       db=argv[3])
+    """ lists states from hbtn_0a_usa db """
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=argv[1],
+                         password=argv[2],
+                         db=argv[3])
 
-  """ Crating Cursor for database"""
-  cur = db.cursor()
+    """ Crating Cursor for database"""
+    cur = db.cursor()
 
-  """ Instruction to the database """
-  cur.execute("SELECT * FROM states ORDER BY id ASC")
+    """ Instruction to the database """
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
 
-  for row in cur.fetchall(): #printing
-    print(row)
+    for row in cur.fetchall():  # printing
+        print(row)
 
-  """ Clean Up """
-  cur.close()
-  db.close()
+    """ Clean Up """
+    cur.close()
+    db.close()
