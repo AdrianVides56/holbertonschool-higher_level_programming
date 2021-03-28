46c49775bcc7bf1325c490c779aaa65e6f0b722d
AdrianVides56 <adrianvj18@gmail.com>
2021-03-27 21:59:58 -0500

    Added task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Importing """
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    """ lists all states with a name starting with N """
+    db = MySQLdb.connect(host='localhost',
+                         user=argv[1],
+                         password=argv[2],
+                         database=argv[3],
+                         port=3306)
+
+    """ Creating cursor """
+    cur = db.cursor()
+
+    """ Instruction to the database """
+    cur.execute("SELECT * FROM states WHERE\
+    name REGEXP '^N.*' ORDER BY id ASC")
+    for row in cur.fetchall():
+        print(row)
+
+    """ Clean up """
+    cur.close()
+    db.close()
