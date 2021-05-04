a0fbd560f2b89dec4b318bfa7f11fa4758d55b2a
AdrianVides56 <adrianvj18@gmail.com>
2021-05-04 12:05:22 -0500

    Added task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const fs = require('fs');
+const myFile = process.argv[2];
+
+fs.readFile(myFile, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
