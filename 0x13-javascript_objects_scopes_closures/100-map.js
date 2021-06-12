45691cc2d1036ab272a327fdbf7a3d6e131578ff
AdrianVides56 <adrianvj18@gmail.com>
2021-06-12 15:46:53 -0500

    Changed map

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
-
-const newList = list.map(x => x * list.indexOf(x));
+let index = 0;
+const newList = list.map(x => x * index++);
 
 console.log(list);
 console.log(newList);
