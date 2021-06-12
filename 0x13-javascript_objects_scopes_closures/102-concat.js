3ee9174edcf042d7b14d293e7544a1d400605a65
AdrianVides56 <adrianvj18@gmail.com>
2021-06-12 17:04:53 -0500

    Removed new line between two strings

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const fileA = fs.readFileSync(process.argv[2], 'utf8');
 const fileB = fs.readFileSync(process.argv[3], 'utf8');
 try {
-  fs.writeFileSync(process.argv[4], fileA + '\n' + fileB);
+  fs.writeFileSync(process.argv[4], fileA + fileB);
 } catch (err) {
   console.error(err);
 }
