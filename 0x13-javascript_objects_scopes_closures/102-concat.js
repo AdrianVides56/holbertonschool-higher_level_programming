2c428bd89c2abb7d5ed8184d539af6971f6089d2
AdrianVides56 <adrianvj18@gmail.com>
2021-06-12 16:54:46 -0500

    removed last blank line

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,15 +1,25 @@
 #!/usr/bin/node
 const fs = require('fs');
+let a = 0;
 function read (num) {
   fs.readFile(process.argv[num], 'utf8', (err, data) => {
     if (err) {
       console.error(err);
     } else {
-      fs.appendFile(process.argv[4], data + '\n', err => {
-        if (err) {
-          console.error(err);
-        }
-      });
+      if (a === 0) {
+        fs.appendFile(process.argv[4], data + '\n', err => {
+          if (err) {
+            console.error(err);
+          }
+        });
+        a = 1;
+      } else {
+        fs.appendFile(process.argv[4], data, err => {
+          if (err) {
+            console.error(err);
+          }
+        });
+      }
     }
   });
 }
