1f45132416e8af8accd308b6201bc9d34a17509e
AdrianVides56 <adrianvj18@gmail.com>
2021-06-12 17:03:22 -0500

    restructured code

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,28 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
-let a = 0;
-function read (num) {
-  fs.readFile(process.argv[num], 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-    } else {
-      if (a === 0) {
-        fs.appendFile(process.argv[4], data + '\n', err => {
-          if (err) {
-            console.error(err);
-          }
-        });
-        a = 1;
-      } else {
-        fs.appendFile(process.argv[4], data, err => {
-          if (err) {
-            console.error(err);
-          }
-        });
-      }
-    }
-  });
+const fileA = fs.readFileSync(process.argv[2], 'utf8');
+const fileB = fs.readFileSync(process.argv[3], 'utf8');
+try {
+  fs.writeFileSync(process.argv[4], fileA + '\n' + fileB);
+} catch (err) {
+  console.error(err);
 }
-
-read(2);
-read(3);
